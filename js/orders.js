19cfe65f8f57f71bd8c4c4af7e0999b393ff9fd1
Ansiedad <98248758+Ansiedad07@users.noreply.github.com>
2023-01-23 21:40:07 -0600

    Add files via upload

---
+++ js/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'Arreglado'
+    },
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'Arreglado'
+    },
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'Arreglado'
+    },
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'Arreglado'
+    },
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'Arreglado'
+    },
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'Arreglado'
+    },
+    {
+        productName: 'Arreglar bug',
+        productNumber: '22/12/10',
+        paymentStatus: '10pm',
+        shipping: 'No arreglado'
+    }
+]
